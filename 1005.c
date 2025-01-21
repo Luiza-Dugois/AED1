ccd4879155357c713d76570fa48a6eb84ea8882b
Luiza Dugois <luiza.dugois@icloud.com>
2025-01-21 14:54:58 -0300

    Create 1005.c

---
+++ 1005.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float A,B, X;
+    scanf("%f", &A);
+    scanf("%f", &B);
+    X=(A*3.5+B*7.5)/11;
+    printf("MEDIA = %0.5f\n", X);
+    return 0;
+}
